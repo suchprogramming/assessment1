a3e1961159ef3354c69a1c537baaee7370941044
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-09-02 17:54:44 -0700

    update styles and add js libraries

---
+++ public/js/scripts.js
@@ -0,0 +1,42 @@
+$(function() {
+
+  console.log( "ready!" );
+
+  $("#user_sentence").val("");
+  $("#word_to_check").val("");
+
+  $("#word-checker-button").click(function() {
+    event.preventDefault();
+  });
+
+  var checkSentence;
+  var checkWord;
+
+  $("#main-form").keyup(function () {
+
+    $("#user_sentence").keyup(function () {
+      checkSentence = $("#user_sentence").val().trim();
+      console.log(checkSentence)
+      checkSentence != "" ? $("#sentence-input").removeClass("has-error").addClass("has-success") : $("#sentence-input").removeClass("has-success").addClass("has-error")
+    })
+
+    $("#word_to_check").keyup(function () {
+      checkWord = $("#word_to_check").val().trim();
+      console.log(checkWord)
+      checkWord != "" ? $("#word-input").removeClass("has-error").addClass("has-success") : $("#word-input").removeClass("has-success").addClass("has-error")
+    })
+
+    if (checkWord && checkSentence != "") {
+      $("#word-checker-button").removeClass("disabled");
+      $("#word-checker-button").click(function() {
+        $("#main-form").submit();
+      });
+    } else {
+      $("#word-checker-button").addClass("disabled");
+      $("#word-checker-button").unbind("click")
+      $("#word-checker-button").click(function() {
+        event.preventDefault();
+      });
+    }
+  })
+});
